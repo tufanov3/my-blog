ccef30917dd20e955c2cf96cff954475e3ced6ca
Geidar <tufanov.geidar@mail.ru>
2025-01-16 14:43:06 +0300

    vercel render

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import Container from "@mui/material/Container";
 import { Routes, Route } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
